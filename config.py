fd8cf5269a60ac4066fd8b61a88df57405311a22
Abdelrahman Shehata <ams311@duke.edu>
2023-10-03 22:08:28 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+import base64
+CLIENT_ID = '6b7e2e58b62d4010a958a1f14c3a2319'
+CLIENT_SECRET = 'fcffd00829104392be509b4531c95c5d'
+REDIRECT_URI = 'http://127.0.0.1:5000/callback/'
+SCOPE = 'user-top-read playlist-read-private user-library-read'
+AUTHORIZATION = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode('utf-8')).decode('utf-8')
