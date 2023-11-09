06492cf4b5440a1f66f09ada721e5f7dac78fd45
abdelrahmanshehata <ams311@duke.edu>
2023-11-08 21:19:45 -0500

    - Update Howtouse notebook with instructions for new features - Introduce Playlist and Song databases for better data management - Modify config file to accommodate new database connections - Create User database to store user-specific information - Add new HTML templates for playlist blending functionality:   - blend_playlists.html for initiating blending process   - blend_with_user_form.html for user input form   - playlist_created.html to confirm playlist creation   - show_blended_playlist.html to display the final blended playlist - Update existing HTML templates to integrate new features

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import base64
 CLIENT_ID = '6b7e2e58b62d4010a958a1f14c3a2319'
 CLIENT_SECRET = 'fcffd00829104392be509b4531c95c5d'
 REDIRECT_URI = 'http://127.0.0.1:5000/callback/'
-SCOPE = 'user-top-read playlist-read-private user-library-read'
+SCOPE = 'user-top-read playlist-read-private user-library-read playlist-modify-public playlist-modify-private'
 AUTHORIZATION = base64.b64encode(f"{CLIENT_ID}:{CLIENT_SECRET}".encode('utf-8')).decode('utf-8')
